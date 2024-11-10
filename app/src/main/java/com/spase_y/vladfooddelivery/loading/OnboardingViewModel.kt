d07c48ea7daba6bd9bbde911b16e1bee3c4419cb
Vlad <bboyvladi1989@mail.ru>
2024-11-10 22:01:57 +0300

    new ViewModel
    1.добавил ViewModel и применил в MainActivity. если пользователь зарегестрирован, то покажется меню. если нет, то появится bottomSheet с выбором/регистрацией

---
+++ app/src/main/java/com/spase_y/vladfooddelivery/loading/OnboardingViewModel.kt
@@ -0,0 +1,20 @@
+package com.spase_y.vladfooddelivery.loading
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+
+class OnboardingViewModel(application: Application):AndroidViewModel(application) {
+    private val sharedPreferences = application.getSharedPreferences("user_preferences", Context.MODE_PRIVATE)
+
+    fun isUserLoggedIn(): Boolean {
+        val email = sharedPreferences.getString("user_email",null)
+        return !email.isNullOrEmpty()
+    }
+    fun saveUserEmail(email: String){
+        sharedPreferences.edit().putString("user_email",email).apply()
+    }
+    fun clearUserData(){
+        sharedPreferences.edit().remove("user_email").apply()
+    }
+}
--- app/src/main/java/com/spase_y/vladfooddelivery/root/MainActivity.kt
+++ app/src/main/java/com/spase_y/vladfooddelivery/root/MainActivity.kt
@@ -3,10 +3,17 @@ package com.spase_y.vladfooddelivery.root
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import com.spase_y.vladfooddelivery.loading.main.LoadingFragment
 import com.spase_y.vladfooddelivery.R
+import com.spase_y.vladfooddelivery.loading.OnboardingViewModel
+import com.spase_y.vladfooddelivery.loading.main.MainBottomSheetFragment
+import com.spase_y.vladfooddelivery.main.menu.ui.presentation.MenuFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: OnboardingViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,5 +26,25 @@ class MainActivity : AppCompatActivity() {
                 .commit()
 
         }
+        viewModel = ViewModelProvider(this).get(OnboardingViewModel::class.java)
+
+        if (viewModel.isUserLoggedIn()){
+            showMenu()
+        } else {
+            showMainBottomSheetFragment()
+        }
+    }
+    private fun showMenu() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.main, MenuFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    private fun showMainBottomSheetFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.main, LoadingFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
