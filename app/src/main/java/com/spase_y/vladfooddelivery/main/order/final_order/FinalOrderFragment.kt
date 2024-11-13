5219f35b7c400e5bffc4f4a4215c0d3fa5337f3b
Vlad <bboyvladi1989@mail.ru>
2024-11-14 00:37:42 +0300

    Верста fragment_final_order

---
+++ app/src/main/java/com/spase_y/vladfooddelivery/main/order/final_order/FinalOrderFragment.kt
@@ -0,0 +1,23 @@
+package com.spase_y.vladfooddelivery.main.order.final_order
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.spase_y.vladfooddelivery.R
+
+
+
+
+class FinalOrderFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_final_order, container, false)
+    }
+}
