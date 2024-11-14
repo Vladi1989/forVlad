2eda6db57056ab8c01dffcea0bed2f4f6688da52
Vlad <bboyvladi1989@mail.ru>
2024-11-14 14:55:14 +0300

    Переписал DetailsFragment на binding

--- app/src/main/java/com/spase_y/vladfooddelivery/main/menu/ui/presentation/DetailsFragment.kt
+++ app/src/main/java/com/spase_y/vladfooddelivery/main/menu/ui/presentation/DetailsFragment.kt
@@ -6,16 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.spase_y.vladfooddelivery.R
+import com.spase_y.vladfooddelivery.databinding.FragmentDetailsBinding
 
 
 class DetailsFragment : Fragment() {
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentDetailsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
