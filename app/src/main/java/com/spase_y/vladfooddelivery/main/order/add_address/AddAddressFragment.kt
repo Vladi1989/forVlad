b882e7401ab9519c7bbc5fc49e8b1d3f7f0c5ad9
Vlad <bboyvladi1989@mail.ru>
2024-11-14 14:40:50 +0300

    AddAddressFragment переписал на binding и добавил реакцию на кнопку назад

--- app/src/main/java/com/spase_y/vladfooddelivery/main/order/add_address/AddAddressFragment.kt
+++ app/src/main/java/com/spase_y/vladfooddelivery/main/order/add_address/AddAddressFragment.kt
@@ -6,14 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.spase_y.vladfooddelivery.R
+import com.spase_y.vladfooddelivery.databinding.FragmentAddAddressBinding
 
 
 class AddAddressFragment : Fragment() {
+    private var _binding: FragmentAddAddressBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentAddAddressBinding.inflate(inflater,container,false)
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_add_address, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ivArrowBack3.setOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
