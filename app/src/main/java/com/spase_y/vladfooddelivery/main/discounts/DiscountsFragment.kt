cf1d8a343d079328977501d2a832f9f74ecb5d68
Vlad <bboyvladi1989@mail.ru>
2024-11-14 14:53:12 +0300

    Переписал DiscountsFragment на binding

--- app/src/main/java/com/spase_y/vladfooddelivery/main/discounts/DiscountsFragment.kt
+++ app/src/main/java/com/spase_y/vladfooddelivery/main/discounts/DiscountsFragment.kt
@@ -6,16 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.spase_y.vladfooddelivery.R
+import com.spase_y.vladfooddelivery.databinding.FragmentDiscountsBinding
 
 
 class DiscountsFragment : Fragment() {
+    private var _binding: FragmentDiscountsBinding? = null
+    private val binding get() = _binding!!
+
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentDiscountsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_discounts, container, false)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
