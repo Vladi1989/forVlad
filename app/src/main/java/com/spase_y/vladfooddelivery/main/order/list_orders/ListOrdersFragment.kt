91cc7f932ccafb2b61f04a66e1be2e003e1568a4
Vlad <bboyvladi1989@mail.ru>
2024-11-14 14:59:15 +0300

    Переписал ListOrdersFragment на binding

--- app/src/main/java/com/spase_y/vladfooddelivery/main/order/list_orders/ListOrdersFragment.kt
+++ app/src/main/java/com/spase_y/vladfooddelivery/main/order/list_orders/ListOrdersFragment.kt
@@ -6,16 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.spase_y.vladfooddelivery.R
+import com.spase_y.vladfooddelivery.databinding.FragmentListOrdersBinding
 
 
 class ListOrdersFragment : Fragment() {
+    private var _binding: FragmentListOrdersBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list_orders, container, false)
+        _binding = FragmentListOrdersBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
